1fed8a41d9a0079d098273a9ffc91e1a36f84bd9
Abdulmazid24 <a.m.abdulmazid24@gmail.com>
2025-12-25 17:09:01 +0600

    Configure production API URL for deployment

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:5000/api';
+export const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 export const USERS_URL = '/users';
 export const PRODUCTS_URL = '/products';
 export const ORDERS_URL = '/orders';
